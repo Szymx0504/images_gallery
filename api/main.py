4a2ca1b13acd739bc04c572ec3f6aef1c00aa9af
Szymon Jankowski <szymonjankovski480@gmail.com>
2023-06-10 09:54:12 +0200

    added request to the unsplash api

--- api/main.py
+++ api/main.py
@@ -1,4 +1,17 @@
-from flask import Flask
+import os
+import requests
+from flask import Flask, request
+from dotenv import load_dotenv
+
+# I needed to add newly installed dependencies to Pipfile myself (remember about it)
+
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+
+if not UNSPLASH_KEY: # stop application from running since it's mandatory for the project
+    raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
 
 app = Flask(__name__)
 
@@ -6,5 +19,20 @@ app = Flask(__name__)
 def hello():
     return "Hello world!"
 
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query") # a simple way to get a query parameter (notice that "query" here is actually a name of the argument in this case)
+    headers = {
+        "Authorization": f"Client-ID {UNSPLASH_KEY}",
+        "Accept-Version": "v1"
+    }
+    params = {"query": word}
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data # since it's already in json format
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
+    # cd .. returns you back to the previous file!
+
+# 252525, FFFDFA
