f7687cfb4b88a742357b615340767d243e761460
Szymon Jankowski <szymonjankovski480@gmail.com>
2023-06-12 21:18:29 +0200

    docker-compose for frontend and api

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -10,9 +10,9 @@ const Welcome = () => (
     </p>
     <hr className="my-4"></hr>
     {/* <p> */}
-      {/* <Button bsStyle="primary" href="https://unsplash.com" target="_blank">Learn more</Button>  */} {/* target="_blank" specifies that the link will be opened in a new tab */}
+    {/* <Button bsStyle="primary" href="https://unsplash.com" target="_blank">Learn more</Button>  */} {/* target="_blank" specifies that the link will be opened in a new tab */}
     {/* </p> */}
-    <a className="btn btn-primary btn-lg" href="https://unsplash.com" role="button" target="_blank">Learn more</a>
+    <a className="btn btn-primary btn-lg" href="https://unsplash.com" role="button" target="_blank" rel="noreferrer">Learn more</a>
   </div>
 );
 
