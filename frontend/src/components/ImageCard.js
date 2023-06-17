d1669f6b5e0d098e12dc0b9c2bdd820c289904d0
Szymon Jankowski <szymonjankovski480@gmail.com>
2023-06-17 11:14:50 +0200

    image author info

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,14 +1,20 @@
 import React from "react";
 // import Button from 'react-bootstrap/Button';
 // import Card from 'react-bootstrap/Card';
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Nav } from "react-bootstrap";
 
 const buttonSpacing = {
   justifyContent: "center",
   textAlign: "center"
 }
+const centerText = {
+  "textAlign": "center",
+  "justifyContent": "center"
+}
 
 const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name || "No author name given";
+  const authorPortfolioURL = image.user?.portfolio_url;
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image.urls.small} />
@@ -21,6 +27,10 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
           {image.saved ? null : <Button variant="secondary" onClick={() => saveImage(image.id)}>Save</Button>}
         </div>
       </Card.Body>
+      {/* you could also give it a class: className="text-center" */}
+      <Card.Footer className="text-muted" style={centerText}>
+        {authorPortfolioURL ? <Nav.Link href={authorPortfolioURL} target="_blank">{authorName}</Nav.Link> : <p>{authorName}</p>}
+      </Card.Footer>
     </Card>
   );
 };
